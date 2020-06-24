565583799266f048b8399bf34df1cee86ec38554
Saga Hansson <gushansad@GU.GU.SE@mltgpu.flov.gu.se>
2020-06-24 20:40:56 +0200

    probably done w a2.py

--- a2.py
+++ a2.py
@@ -1,8 +1,20 @@
+## orginal med svd dim reduction
+
 import argparse
 import random
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.base import is_classifier
 import numpy as np
+from nltk.corpus import stopwords
+import collections
+from sklearn.decomposition import TruncatedSVD
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+stopwords = stopwords.words('english')
 random.seed(42)
 
 
@@ -20,16 +32,53 @@ def tokenize_to_list(onesample):
     # creates lists of words that are lowercase and alphabetiacl 
     lower = onesample.lower()
     word_strings = lower.split()
-    only_alpha = [word for word in word_strings if word.isalpha()]
+    only_alpha = [word for word in word_strings if word.isalpha() and word not in stopwords]
     return only_alpha
+
+def word_count(alist, no_of_words, index_dict):
+    #puts counts instead of zeroes in np.zeros
+    count_dict = collections.Counter(alist)
+    vector = np.zeros(no_of_words)
+    
+    for word in alist:
+        i = index_dict[word] # fetches the index of the current word from index_dict aka word_idx 
+        vector[i] = count_dict[word] # puts the word count in the right place w the help of the index
+    return vector
+
 
 
 def extract_features(samples):
     print("Extracting features ...")
+    
+    sample_list = [] # list of lists where each inner list is a doc
+    idx = 0 # needed to place the words in the array
+    word_idx = {} # dict where each word gets an index 
+    for sample in samples:
+        sample_words = tokenize_to_list(sample) # creating list of all tokens for each file
+        sample_list.append(sample_words) 
+        for word in sample_words:
+            if word not in word_idx:
+                word_idx[word] = idx  # giving each word an index in word_idx
+                idx += 1
+    no_of_words = len(word_idx)
+    no_of_docs = len(samples)
+    
+    big_array = np.zeros((no_of_docs, no_of_words)) # creating array with as many columns as there are words, and as many rows as there are docs
 
+    sample_no = 0 # keeps track of which row we're on
+    for sam in sample_list: # i e for every doc
+        big_array[sample_no] = word_count(sam, no_of_words, word_idx) # gets word counts of file, puts them at the right place in the array by looking at word_idx
+        sample_no += 1
+    print('Features extracted.')
+    print('Number of words before filtering: ', big_array.shape[1]) 
     
+    big_arr_sum = np.sum(big_array, axis =0) # gets total word counts
+    array_filter = big_arr_sum > 10 # boolean filter
+    filtered_arr = big_array[:, array_filter] # filters out words that occur less than 10 times in total
     
-    #pass #Fill this in
+    print('Number of words after filtering: ', filtered_arr.shape[1])
+        
+    return filtered_arr
 
 
 
@@ -47,18 +96,21 @@ def part2(X, n_dim):
 
 
 def reduce_dim(X,n=10):
-    #fill this in
-    pass
-
+    svd = TruncatedSVD(n_components=n)
+    print('Fitting and transforming')
+    fit_transformed = svd.fit_transform(X)
+    return fit_transformed 
 
 
 ##### PART 3
 #DONT CHANGE THIS FUNCTION EXCEPT WHERE INSTRUCTED
 def get_classifier(clf_id):
     if clf_id == 1:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = DecisionTreeClassifier()
     elif clf_id == 2:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = GaussianNB()
+
+
     else:
         raise KeyError("No clf with id {}".format(clf_id))
 
@@ -94,18 +146,26 @@ def part3(X, y, clf_id):
 
 
 def shuffle_split(X,y):
-    pass # Fill in this
-
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
 
 def train_classifer(clf, X, y):
     assert is_classifier(clf)
-    ## fill in this
-
+    clf.fit(X, y)
+    
 
 def evalute_classifier(clf, X, y):
     assert is_classifier(clf)
-    #Fill this in
-
+    pred = clf.predict(X)
+    acc = metrics.accuracy_score(y, pred)
+    prec = metrics.precision_score(y, pred, average='weighted')
+    rec = metrics.recall_score(y, pred, average='weighted')
+    f1 = metrics.f1_score(y, pred, average='weighted')
+    print('Accuracy: ', acc)
+    print('Precision: ', prec)
+    print('Recall: ', rec)
+    print('F1-measure: ', f1)
 
 ######
 #DONT CHANGE THIS FUNCTION
@@ -117,6 +177,37 @@ def load_data():
     print("Example label name: ", data.target_names[data.target[0]])
     print("Number of possible labels: ", len(data.target_names))
     return data.data, data.target, data.target_names
+
+### TIMER
+import atexit
+from time import time, strftime, localtime
+from datetime import timedelta
+
+def secondsToStr(elapsed=None):
+    if elapsed is None:
+        return strftime("%Y-%m-%d %H:%M:%S", localtime())
+    else:
+        return str(timedelta(seconds=elapsed))
+
+def log(s, elapsed=None):
+    line = "="*40
+    print(line)
+    print(secondsToStr(), '-', s)
+    if elapsed:
+        print("Elapsed time:", elapsed)
+    print(line)
+    print()
+
+def endlog():
+    end = time()
+    elapsed = end-start
+    log("End Program", secondsToStr(elapsed))
+
+start = time()
+atexit.register(endlog)
+log("Start Program")
+### timer end
+
 
 
 #DONT CHANGE THIS FUNCTION
